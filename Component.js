148c80955aa7255cd442891d2442f37ea3803d86
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-05-11 00:31:13 +0530

    implemented useState() hook

--- Component.js
+++ Component.js
@@ -1,6 +1,12 @@
+import { useState } from "./MyReact.js";
+
 export function Component({ propCount }) {
-  const count = 0;
+  const [count, setCount] = useState(10);
   const propCountDoubled = 0;
+
+  setTimeout(() => {
+    setCount((count) => count + 1);
+  }, 1000);
 
   return `
         State: ${count}
--- MyReact.js
+++ MyReact.js
@@ -1,4 +1,34 @@
+let globalId = 0;
+let globalParent;
+const componentState = new Map();
+
 export function render(component, props, parent) {
-  const element = component(props);
-  parent.innerText = element;
+  const state = componentState.get(parent) || { cache: [] };
+  componentState.set(parent, { ...state, component, props });
+  globalParent = parent;
+  const output = component(props);
+  globalId = 0;
+  parent.innerText = output;
+}
+
+export function useState(initialState) {
+  const id = globalId;
+  const { cache, props, component } = componentState.get(globalParent);
+  if (cache[id] == null) {
+    cache[id] = {
+      value: typeof initialState === "function" ? initialState() : initialState,
+    };
+  }
+
+  globalId++;
+
+  const setState = (value) => {
+    if (typeof value === "function") {
+      cache[id].value = value(cache[id].value);
+    } else {
+      cache[id].value = value;
+    }
+    render(component, props, globalParent);
+  };
+  return [cache[id].value, setState];
 }
